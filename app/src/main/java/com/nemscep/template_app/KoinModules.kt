63101555ef52252603487561f520344638188a5a
nemscep <nemscepanovic@gmail.com>
2022-02-12 20:28:19 +0100

    Introduce network (retrofit, okhttp, moshi) and test related dependencies

--- app/src/main/java/com/nemscep/template_app/KoinModules.kt
+++ app/src/main/java/com/nemscep/template_app/KoinModules.kt
@@ -4,13 +4,48 @@ import com.nemscep.template_app.auth.data.AuthRepositoryImpl
 import com.nemscep.template_app.auth.domain.repository.AuthRepository
 import com.nemscep.template_app.auth.domain.usecases.IsUserLoggedIn
 import com.nemscep.template_app.splash.SplashViewModel
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 /**
  * All koin dependencies.
  */
 val modules = listOf(
+    // Common
+    module {
+        single {
+            Moshi.Builder()
+                .addLast(KotlinJsonAdapterFactory())
+                .build()
+        }
+
+        single {
+            HttpLoggingInterceptor()
+                .apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+        }
+
+        single {
+            OkHttpClient.Builder()
+                .addInterceptor(HttpLoggingInterceptor(logger = get()))
+                .build()
+        }
+
+        single {
+            Retrofit.Builder()
+                .baseUrl("/")
+                .addConverterFactory(MoshiConverterFactory.create(get()))
+                .client(get())
+                .build()
+        }
+    },
     // Main
     module {
         viewModel { MainActivityViewModel() }
